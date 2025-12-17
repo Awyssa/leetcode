1eba2029f579228ca660b1f9773ee87a2ef74f28
Michael <michael.ehson@gmail.com>
2025-12-17 09:39:19 +0000

    I dunno what I did

--- 01-arrays-and-hashing/0049-group-anagrams/go/main.go
+++ 01-arrays-and-hashing/0049-group-anagrams/go/main.go
@@ -1,6 +1,40 @@
 package main
 
-// TODO: Implement solution
+import (
+	"fmt"
+)
 
 func main() {
+	groupAnagrams([]string{"ant", "nat", "apple", "toy", "yot"})
+}
+
+type Address struct {
+	firstLineAddress string
+	city             string
+	postcode         string
+	udprn            int32
+}
+
+type User struct {
+	username string
+	age      int32
+	address  Address
+}
+
+func groupAnagrams(words []string) [][]string {
+
+	user := User{
+		username: "Michael",
+		age:      32,
+		address: Address{
+			firstLineAddress: "19 Passmore Gardens",
+			city:             "London",
+			postcode:         "N11 2PE",
+			udprn:            123456,
+		},
+	}
+
+	fmt.Println(user)
+
+	return nil
 }
--- 01-arrays-and-hashing/0049-group-anagrams/js/index.js
+++ 01-arrays-and-hashing/0049-group-anagrams/js/index.js
@@ -1,24 +1,8 @@
-// const groupAnagrams = (words) => {
-// 	let returnArray = {};
-// 	for(let i = 0; i < words.length; i++) {
-// 		const currentWord = words[i].split("").sort().join("");
-// 		for(let j = 0; j < words.length; j++) {
-// 			const compareWord = words[j].split("").sort().join("");
-// 			if(currentWord === compareWord && i !== j)
-// 			{
-// 				loopArray.push(words[j]);
-// 				words.splice(j, 1);
-// 			}
-// 		}
-// 		returnArray.push(loopArray);
-// 	}
-// 	return returnArray;
-// };
-
 const groupAnagrams = (words) => {
 	let returnObject = {};
 	for (let word of words) {
 		const sortedWord = word.split("").sort().join("");
+
 		if (!returnObject[sortedWord])
 			returnObject[sortedWord] = [word];
 		else
@@ -26,11 +10,3 @@ const groupAnagrams = (words) => {
 	}
 	return Object.values(returnObject);
 };
-
-const value = groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
-const answer = [[["bat"],["nat","tan"],["ate","eat","tea"]]];
-
-console.log(value);
-console.log(answer);
-
-console.log("Did we get it correct? ", value === answer);
